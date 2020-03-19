d71cacae45b78db4d54357ab128a6488f1b5c2fd
agus94 <agusismail94@gmail.com>
2020-03-19 15:52:28 +0700

    laravel-crud rollo academy

---
+++ resources/views/siswa/edit.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.master')
+
+
+@section('content')
+<div class="main">
+    <div class="main-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Inputs</h3>
+                        </div>
+                        <div class="panel-body">
+                            <form action="/siswa/{{$siswa->id}}/edit" method="post">
+                                {{csrf_field()}}
+                                <div class="form-group">
+                                    <label for="nama_depan">Nama Depan</label>
+                                    <input type="input" name="nama_depan" class="form-control" id="nama_depan" aria-describedby="emailHelp" placeholder="Nama Depan" value="{{$siswa->nama_depan}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="nama_belakang">Nama Belakang</label>
+                                    <input type="input" name="nama_belakang" class="form-control" id="nama_belakang" aria-describedby="emailHelp" placeholder="Nama Belakang" value="{{$siswa->nama_belakang}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
+                                    <select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
+                                    <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki-laki</option>
+                                    <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="agama">Agama</label>
+                                    <input type="input" name="agama" class="form-control" id="agama"  baria-describedby="emailHelp" placeholder="Agama" value="{{$siswa->agama}}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="alamat">Alamat</label>
+                                    <textarea name="alamat" class="form-control" id="alamat" rows="3">{{$siswa->alamat}}</textarea>
+                                </div>
+            
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-warning">Update</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
+
+@section('content1')
+    <h1>Edit Data Siswa</h1>
+    @if(session('sukses'))
+        <div class="alert alert-success" role="alert">
+            {{session('sukses')}}
+        </div>
+    @endif
+
+    <div class="row">
+        <div class="col-lg-12">
+            
+            <form action="/siswa/{{$siswa->id}}/edit" method="post">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <label for="nama_depan">Nama Depan</label>
+                    <input type="input" name="nama_depan" class="form-control" id="nama_depan" aria-describedby="emailHelp" placeholder="Nama Depan" value="{{$siswa->nama_depan}}">
+                </div>
+                <div class="form-group">
+                    <label for="nama_belakang">Nama Belakang</label>
+                    <input type="input" name="nama_belakang" class="form-control" id="nama_belakang" aria-describedby="emailHelp" placeholder="Nama Belakang" value="{{$siswa->nama_belakang}}">
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
+                    <select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
+                    <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki-laki</option>
+                    <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="agama">Agama</label>
+                    <input type="input" name="agama" class="form-control" id="agama"  baria-describedby="emailHelp" placeholder="Agama" value="{{$siswa->agama}}">
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <textarea name="alamat" class="form-control" id="alamat" rows="3">{{$siswa->alamat}}</textarea>
+                </div>
+                
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-warning">Update</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
